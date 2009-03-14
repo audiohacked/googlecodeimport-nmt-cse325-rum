06ec123ce7e6e7e2b46234984d799bfa2c32e80a
coreyb42 <coreyb42@147f70ce-fd4b-11dd-87c9-111d11f6bf44>
2009-03-14 21:19:32 +0000

    Added a uio struct to file descriptor, named 'location'. Mostly implemented file IO, commented where more work needs to be done.

--- src/kern/userprog/file.c
+++ src/kern/userprog/file.c
@@ -12,8 +12,7 @@ extern int errno;
 * and can be ignored.
 *
 * Returns a non-negative file handle, or -1 for error */
-int 
-open(cont char *path, int oflag, mode_t mode)
+int open(cont char *path, int oflag, mode_t mode)
 {
 	/* set up variables for the vnode return, and function call return */
 	int result, fd;
@@ -25,10 +24,16 @@ open(cont char *path, int oflag, mode_t mode)
 	/* use vfs_open to open the file */
 	result = vfs_open( path, oflag, &v);
 	
+	if(curthread->fdcount >= MAX_FD)
+	{
+		errno = EMFILE;
+		return -1;
+	}
+	
 	/* add new fd to process's fd table */
 	fd = curthread->fdcount++;
 	curthread->t_fd[fd].vfs_node = v;
-	if (oflag & O_RDONLY) 
+	if (oflag & O_RDONLY)
 	{
 		curthread->t_fd[fd].writeable=0;
 	}
@@ -36,7 +41,8 @@ open(cont char *path, int oflag, mode_t mode)
 	{
 		curthread->t_fd[fd].writeable=1;
 	}
-
+	curthread->t_fd[fd]->location.uio_offset = 0;
+	
 	return fd;
 }
 
@@ -78,7 +84,31 @@ int close(int fid)
 * Returns number of bytes read, 0 for EOF, or -1 for error */
 int read(int fd, void *buf, size_t buflen)
 {
+	int result;
 	
+	// check for validity of file handle
+	if(curthread->t_fd[fd]->vfs_node == NULL)
+	{
+		errno = EBADF;
+		return -1;
+	}
+	
+	//set flags of uio to match read operation
+	curthread->t_fd[fd]->location.uio_rw = UIO_READ;
+	curthread->t_fd[fd]->location.uio_resid = buflen;
+	
+	result = VOP_READ(curthread->t_fd[fd]->vfs_node, 
+		curthread->t_fd[fd]->location);
+	
+	// if result is 0, we've reached EOF - else move data into buf
+	if(result > 0)
+	{
+		// still need to implement actually copying data to buf
+		// and checking buf for problems
+		//buf = curthread->t_fd[fd]->location->uio_space.iov_un;
+		//change offset here?
+	}
+	return result;
 }
 
 // write data to a file
@@ -92,17 +122,81 @@ int read(int fd, void *buf, size_t buflen)
 * Returns the number of bytes written, or a negative number for an error */
 int write(int fd, const void *buf, size_t nbytes>)
 {
+	int result;
 	
+	/***************** Error Checking ******************************/
+	// if handle is invalid or file is not writable, return an error
+	if( (curthread->t_fd[fd]->vfs_node == NULL) || 
+		(curthread->t_fd[fd].writeable!=1) )
+	{
+		errno = EBADF;
+		return -1;
+	}
+	// make sure buffer is initialized
+	else if(buf == NULL)
+	{
+		errno = EFAULT;
+		return -1;
+	}
+	// make sure there's space on filesystem (not yet sure how to do this)
+	else if(space < nbytes)
+	{
+		errno = ENOSPC;
+		return -1;
+	}
+	/***************** End Error Checking ***************************/
+	
+	// make uio writable
+	curthread->t_fd[fd]->location.uio_rw = UIO_WRITE;
+	// put buffer pointer into uio
+	curthread->t_fd[fd]->location->uio_space.iov_un = buf;
+	// resid is the number of bytes to be written
+	curthread->t_fd[fd]->location.uio_resid = nbytes;
+	
+	result = VOP_WRITE(curthread->t_fd[fd]->vfs_node, 
+	curthread->t_fd[fd]->location);
+	return result;
 }
 
 //change current position in file
 /* lseek alters the current seek position of the file handle filehandle, 
 * seeking to a new position based on pos and whence. 
-*
 * Returns new position or -1 for error */
 off_t lseek(int fd, off_t pos, int whence)
 {
-	
+	// check to make sure we're not seeking past the beginning of the file
+	if(whence == SEEK_CUR && 
+		pos < 0 && pos > curthread->t_fd[fd]->location.uio_offset)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+	// check for invalid file handle
+	else if(curthread->t_fd[fd]->vfs_node == NULL)
+	{
+		errno = EBADF;
+		return -1;
+	}
+	else if(whence == SEEK_SET)
+	{
+		//probably need to check for EOF here
+		//looks like vop_tryseek will help a lot
+		curthread->t_fd[fd]->location.uio_offset = pos;
+		return curthread->t_fd[fd]->location.uio_offset;
+	}
+	else if(whence == SEEK_END)
+	{
+		//do a read to the EOF, then add
+		//pos to offset
+		//need to figure out if vop_read changes the offset,
+		//or I need to change it manually (same goes for read)
+		return curthread->t_fd[fd]->location.uio_offset;
+	}
+	else
+	{
+		errno = EINVAL;
+		return -1;
+	}
 }
 
 // clone a file handle
@@ -141,6 +235,6 @@ int dup2(int oldfd, int newfd)
 	nfd->vfs_node = ofd->vfs_node;
 	nfd->writable = ofd->writable;
 	nfd->readable = ofd->readable;
-	nfd->location = ofd->location;
+	nfd->location.uio_offset = ofd->location.uio_offset;
 	return 0;
 }
