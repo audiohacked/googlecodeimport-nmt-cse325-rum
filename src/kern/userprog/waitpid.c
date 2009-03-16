60aa8c5eab39c378b66ed0f99d01de23697a6d1f
audiohacked <audiohacked@147f70ce-fd4b-11dd-87c9-111d11f6bf44>
2009-03-16 18:50:01 +0000

    Implement waitpid and extend pid system for waitpid.

---
+++ src/kern/userprog/waitpid.c
@@ -0,0 +1,33 @@
+pid_t
+waitpid(pid_t wpid, int *status, int options)
+{
+	struct proc_table *ptable;
+	int ret;
+
+	if (status == NULL)
+	{
+		errno = EFAULT;
+		return -1;
+	}
+	
+	if (array_getnum(process_table) < wpid)
+	{
+		errno = EFAULT;
+		return -1;
+	}
+
+	ptable = array_getguy(process_table, wpid);
+
+	switch (options)
+	{	
+	0:
+		break;
+	default:
+		errno = EINVAL;
+		return -1;
+	}
+
+	while(!ptable->exited);
+	*status = ptable->exit_code;
+	return ptable->process_id;
+}
